8b8481b13e2aced0ff091f8b708d953706f5004b
Junyu Yan <jyan6@studentmail.nait.ca>
2015-09-17 14:07:09 -0600

    add Git-Exercise1

---
+++ Git_Exercise1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Git_Exercise1.Startup))]
+namespace Git_Exercise1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
